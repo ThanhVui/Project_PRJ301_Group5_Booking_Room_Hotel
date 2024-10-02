6b8fa4e982a2b3ec8be74d873cfa8e67ae08025a
thanhvui <thanhvuikg100@gmail.com>
2024-10-02 22:53:30 +0700

    Template Update 01

---
+++ Project_PRJ301_Group5_Booking_Room_Hotel/src/main/java/Utils/DBContext.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Tang Thanh Vui - CE180901
+ */
+public class DBContext {
+
+    protected Connection connection;
+
+    public DBContext() throws SQLException {
+        try {
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=NameOfDatabase;trustServerCertificate=true";
+            String username = "sa";
+            String password = "123456";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
